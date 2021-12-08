cc291190cf89edf29da7b73272f4b31b595b294f
Cole <colemooney5@gmail.com>
2021-12-08 10:30:43 -0800

    Updates

---
+++ precache-manifest.75a873a9debf9347d3c164fd38c6811d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844c61e91dd99aa05933259fa50420c6",
+    "url": "/colemooney/MernCrudApp/index.html"
+  },
+  {
+    "revision": "85fb9fdbb3fb0110766b",
+    "url": "/colemooney/MernCrudApp/static/css/main.db0b737a.chunk.css"
+  },
+  {
+    "revision": "690412fc2755055a8574",
+    "url": "/colemooney/MernCrudApp/static/js/2.229af79f.chunk.js"
+  },
+  {
+    "revision": "494afc703f711ec49c1c71444dc0dfb0",
+    "url": "/colemooney/MernCrudApp/static/js/2.229af79f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85fb9fdbb3fb0110766b",
+    "url": "/colemooney/MernCrudApp/static/js/main.f5a59018.chunk.js"
+  },
+  {
+    "revision": "98423f1ea3907469fc7b",
+    "url": "/colemooney/MernCrudApp/static/js/runtime-main.15272d2d.js"
+  }
+]);
